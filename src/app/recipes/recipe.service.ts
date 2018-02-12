bba8dc33ada6e67a564c0c903e53b61926a9025b
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-12 17:53:55 +0300

    Use recipe service to communicate between recipe-item and recipes components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'http://www.bluepoppy.com/images/TAOC.jpg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'http://www.bluepoppy.com/images/TAOC.jpg')
