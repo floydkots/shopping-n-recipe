7bbc4b833f66aa472292f59dc45b49bec994e759
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-13 14:03:55 +0300

    Navigate to recipe edit page programmatically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import {RecipeService} from '../recipe.service';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { relativeToRootDirs } from '@angular/compiler-cli/src/transformers/util';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +11,15 @@ import {RecipeService} from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route}) ;
   }
 }
